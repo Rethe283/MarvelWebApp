f17f7df113cd8977cce41d8febf31dff35a5f54f
Nikolaos Spiliopoulos <Spiliopn@gmail.com>
2021-05-12 09:35:46 +0300

    Heroes List initial

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -23,6 +23,10 @@ namespace MarvelWebApp.Controllers
         {
             return View();
         }
+        public IActionResult Heroes()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
