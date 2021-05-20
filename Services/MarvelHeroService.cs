70d27bcdde37dd8e4fb163756dbb15e33e85370d
Nikolaos Spiliopoulos <Spiliopn@gmail.com>
2021-05-20 19:58:06 +0300

    md5 digest calculated and marvel api responded status code 200 ("OK")

--- Services/MarvelHeroService.cs
+++ Services/MarvelHeroService.cs
@@ -3,7 +3,10 @@ using MarvelWebApp.Models;
 using Microsoft.Extensions.Options;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Net.Http;
+using System.Security.Cryptography;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace MarvelWebApp.Services
@@ -22,7 +25,10 @@ namespace MarvelWebApp.Services
         public async Task<IEnumerable<Hero>> GetHeroes()
         {
             var timestamp = DateTimeOffset.UtcNow.ToUnixTimeMilliseconds();
-            var response = await HttpClient.GetAsync($"v1/public/characters?ts={timestamp}&apikey={ApiSettings.PublicKey}&hash=123");
+            var tohash = timestamp + ApiSettings.PrivateKey + ApiSettings.PublicKey;
+            var hash = MD5.Create();
+            var hashoutput = string.Join(string.Empty, hash.ComputeHash(Encoding.UTF8.GetBytes(tohash)).Select(b => b.ToString("x2")));
+            var response = await HttpClient.GetAsync($"v1/public/characters?ts={timestamp}&apikey={ApiSettings.PublicKey}&hash={hashoutput}");
             if (!response.IsSuccessStatusCode)
             {
             }
